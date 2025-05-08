a064f0a8de068641ab2442ca6bc77366f262c6b7
Mennaa6 <mennaabdelfattahhh@gmail.com>
2025-05-08 21:19:29 +0300

    modifications

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+require("dotenv").config();
+const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
+
+const DB = process.env.DB;
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use("/", (req, res) => {
   return res.status(200).json({
