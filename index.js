873642a5afea8d25c158299a5b42ca0a857fd477
Mennaa6 <mennaabdelfattahhh@gmail.com>
2025-05-08 23:15:08 +0300

    added users routes and schema

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const router = require("./routes/products");
 const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
+const router2 = require("./routes/users");
 app.use(cors());
 app.use(express.json());
 
@@ -24,7 +25,7 @@ mongoose
 // });
 
 app.use("/api/products", router);
-
+app.use("/api/users", router2);
 app.use((req, res) => {
   return res.status(500).json({
     status: 500,
